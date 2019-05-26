628e7f34f41afcd7b2c63372418569040f4c4f5d
Kaz Wesley <kaz@lambdaverse.org>
2019-05-26 15:05:52 -0700

    perl6: add zef

--- default.nix
+++ default.nix
@@ -2,4 +2,5 @@ self: super: {
   moar = super.callPackage ./perl6/moar.nix { };
   nqp = super.callPackage ./perl6/nqp.nix { };
   rakudo = super.callPackage ./perl6/rakudo.nix { };
+  zef = super.callPackage ./perl6/zef.nix { };
 }
---
+++ perl6/zef.nix
@@ -0,0 +1,24 @@
+{ stdenv, fetchFromGitHub, rakudo, makeWrapper }:
+
+stdenv.mkDerivation rec {
+  name = "zef-${version}";
+  version = "0.7.4";
+  src = fetchFromGitHub {
+    owner = "ugexe";
+    repo = "zef";
+    rev = version;
+    sha256 = "054jwdl4v26nfic8yi2jbrb6i5zly2w9bjdag1vk9j1961gdsjpz";
+  };
+
+  buildInputs = [ rakudo makeWrapper ];
+  installPhase = ''mkdir -p $out; HOME=$TMPDIR perl6 -I. bin/zef -to="inst#$out" install .'';
+  postFixup = ''for x in $out/bin/*; do wrapProgram $x --set PERL6LIB "inst#$out"; done'';
+
+  meta = with stdenv.lib; {
+    description = "Perl6 Module Management";
+    homepage    = https://github.com/ugexe/zef;
+    license     = licenses.artistic2;
+    platforms   = platforms.unix;
+    maintainers = with maintainers; [ kazcw ];
+  };
+}
